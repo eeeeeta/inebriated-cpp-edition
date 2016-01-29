5fd036feb223a53be584bc6a1287641e58c7913c
eeeeeta <eeeeeta@users.noreply.github.com>
2016-01-29 18:59:05 +0000

    Custom data structure & iterator, cmake stuff
    holy crap, we're not just using standard stuff now!
    we now use an unordered_map<string, Key *>, where Keys are
    linked lists containing possible values, with their weight
    (so we can algorithmerize the crap out of this later).
    
    I also implemented an iterator for all of this. Boy, that took ages.
    
    We now also have cmake support, because that's probably
    going to start being required at some point and it's better
    than a Makefile calling clang++...

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	clang++ -std=c++11 markov.cpp -o markov
-debug:
-	clang++ -std=c++11 -g markov.cpp -o markov
-
--- markov.cpp
+++ markov.cpp
@@ -8,21 +8,86 @@
 #include <cstdlib>
 #include <ctime>
 using string = std::string;
+template <typename T> class KeyIter : public std::iterator<std::forward_iterator_tag, T> {
+public:
+    T* ptr;
+    using difference_type = int;
+    KeyIter(T* iptr) {
+        ptr = iptr;
+    }
+    ~KeyIter() {};
+    KeyIter* operator++(void) {
+        ptr = ptr->next;
+        return this;
+    }
+    bool operator==(KeyIter a) {
+        return a.ptr == ptr;
+    }
+    bool operator!=(KeyIter a) {
+        return a.ptr != ptr;
+    }
+    KeyIter* operator*() {
+        return this;
+    }
+};
+class Key {
+public:
+    using iterator = KeyIter<Key>;
+
+    Key *next;
+    string val;
+    int weight;
+
+    Key(string istr) {
+        val = istr;
+        weight = 0;
+        next = nullptr;
+    }
+    ~Key() {
+        if (next != nullptr) delete next;
+    }
+    Key *grow(string istr) {
+        Key *kp = this;
+        while (kp->next != nullptr) kp = kp->next;
+        kp->next = new Key {istr};
+        return kp->next;
+    }
+    iterator advance(int n) {
+        auto iter = iterator {this};
+        while (n-- != 0) ++iter;
+        return iter;
+    }
+    iterator begin(void) {
+        return iterator {this};
+    }
+    iterator end(void) {
+        return iterator {nullptr};
+    }
+};
 class MarkovDB {
-    std::unordered_multimap<string,string>* kvdb;
+    std::unordered_map<string,Key *>* kvdb;
     std::vector<string> *ssdb;
 public:
     MarkovDB() {
-        kvdb = new std::unordered_multimap<string,string> {};
+        kvdb = new std::unordered_map<string,Key *> {};
         ssdb = new std::vector<string> {};
         std::srand(std::time(0));
     }
     ~MarkovDB() {
+        for (auto iter : *kvdb) {
+            delete iter.second;
+        }
         delete kvdb;
         delete ssdb;
     }
     void insertHunk(string k, string v) {
-        kvdb->insert({k, v});
+        auto iter = kvdb->find(k);
+        if (iter != kvdb->end()) {
+            (iter->second)->grow(v);
+        }
+        else {
+            kvdb->insert({k, new Key {v}});
+        }
         std::cout << "MDB: inserted hunk " << k << " = " << v << "\n";
     }
     void insertSentenceStarter(string ss) {
@@ -30,33 +95,28 @@ public:
         std::cout << "MDB: inserted sentence starter " << ss << "\n";
     }
     string build_sentence(void) {
-        if (ssdb->size() == 0) throw std::invalid_argument("no sentence starters yet");
-        int ridx = std::rand() % ssdb->size();
-        string ss = ssdb->at(ridx);
-        string ret = ss;
-        std::cout << "MDB: got sentence starter: " << ss << "\n";
-        auto iters = kvdb->equal_range(ss);
-        while (iters.first != kvdb->end()) {
-            ridx = std::rand() % std::distance(iters.first, iters.second);
-            int cidx = 0;
-            for (auto iter = iters.first; iter != iters.second; ++iter) {
-                if (cidx == ridx) {
-                    std::cout << "MDB: add hunk " << iter->second << "\n";
-                    ret = ret + " " + iter->second;
-                    ss = iter->second;
-                    break;
-                }
-                cidx++;
-            };
-            iters = kvdb->equal_range(ss);
-        };
-        std::cout << "MDB: sentence: " << ret << "\n";
+        if (ssdb->size() == 0)
+            throw std::invalid_argument("No sentence starters in database.");
+        string startk = ssdb->at(std::rand() % ssdb->size());
+        string ret = startk;
+        std::cout << "MDB: got sentence starter: " << startk << "\n";
+        auto iter = kvdb->find(startk);
+        while (iter != kvdb->end()) {
+            int dist = std::distance(iter->second->begin(), iter->second->end());
+            int rn = std::rand() % dist;
+            std::cout << "MDB: dist " << dist << " rn " << rn << "\n";
+            string str = iter->second->advance(rn).ptr->val;
+            std::cout << "MDB: str " << str << "\n";
+            ret = ret + " " + str;
+            iter = kvdb->find(str);
+        }
+        std::cout << "MDB: built sentence " << ret << "\n";
         return ret;
     }
 };
 
 int main() {
-    std::cout << "inebriated, C++ edition v0.1\nan eta thing <http://theta.eu.org>\n\n";
+    std::cout << "inebriated, C++ edition v0.1.2\nan eta thing <http://theta.eu.org>\n\n";
     MarkovDB mdb {};
     std::cout << "input sentences, newline-separated:\n";
     std::string sentence;

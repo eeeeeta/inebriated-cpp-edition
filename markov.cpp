d8f178f960bee072ea69500bf069dafa8e9db263
eeeeeta <eeeeeta@users.noreply.github.com>
2016-01-29 20:14:35 +0000

    Weight calculation, and Doxygen docs
    Please note that the weight calculations aren't actually in use...
    ...yet.
    
    When I implement something to use them, I'll also consider
    recursion - that seems to be a problem with what I've tried so far.

--- markov.cpp
+++ markov.cpp
@@ -8,6 +8,10 @@
 #include <cstdlib>
 #include <ctime>
 using string = std::string;
+/**
+ * Iterator for Key.
+ * Should be rather simple and self-explanatory.
+ */
 template <typename T> class KeyIter : public std::iterator<std::forward_iterator_tag, T> {
 public:
     T* ptr;
@@ -30,28 +34,58 @@ public:
         return this;
     }
 };
+/**
+ * Linked list, used as values in the Markov database.
+ *
+ * To be able to store both values (string of next key) and
+ * attributes (weightings for better sentence selection), we use
+ * this class.
+ */
 class Key {
 public:
     using iterator = KeyIter<Key>;
 
-    Key *next;
-    string val;
-    int weight;
+    Key *next; /**< next Key in linked list */
+    string val; /**< value string */
+    int weight; /**< weighting, determined by amount of values */
+    int cweight; /**< weighting, determined by amount of repeat insertions (frequency) */
 
     Key(string istr) {
         val = istr;
-        weight = 0;
+        weight = cweight = 0;
         next = nullptr;
     }
+/**
+ * Destructor. Deletes next Key in linked list, if available.
+ *
+ * This has a rather neat 'cascading destruction' effect: this calls
+ * `delete` on `next`, which activates its constructor, which calls `delete`
+ * on its `next`...
+ */
     ~Key() {
         if (next != nullptr) delete next;
     }
+/**
+ * Grow the linked list, adding value `istr`.
+ * @param istr Value to add to list of values.
+ */
     Key *grow(string istr) {
         Key *kp = this;
-        while (kp->next != nullptr) kp = kp->next;
+        while (kp->next != nullptr) {
+            if (kp->val == istr) {
+                kp->cweight++;
+                std::cout << "MDB: was growing " << istr << ", but found existing\n";
+                return kp;
+            }
+            kp = kp->next;
+        }
         kp->next = new Key {istr};
         return kp->next;
     }
+/**
+ * Returns an iterator to the nth item in the linked list.
+ * @param n item to look up
+ */
     iterator advance(int n) {
         auto iter = iterator {this};
         while (n-- != 0) ++iter;
@@ -65,8 +99,26 @@ public:
     }
 };
 class MarkovDB {
-    std::unordered_map<string,Key *>* kvdb;
-    std::vector<string> *ssdb;
+    std::unordered_map<string,Key *>* kvdb; /**< string to Key map */
+    std::vector<string> *ssdb; /**< sentence-starter database */
+
+/**
+ * Recalculate weightings for one 'strand' (linked list) of Key objects.
+ */
+    void _recalcOneStrand(Key *strand) {
+        for (auto k : *(strand)) {
+            std::cout << "MDB: value " << k->ptr->val << ":\n";
+            auto iter = kvdb->find(k->ptr->val);
+            if (iter == kvdb->end()) {
+                k->ptr->weight = 0;
+                std::cout << "- no results\n";
+                return;
+            }
+            int dist = std::distance(iter->second->begin(), iter->second->end());
+            std::cout << "- " << dist << " results\n";
+            k->ptr->weight = dist;
+        }
+    }
 public:
     MarkovDB() {
         kvdb = new std::unordered_map<string,Key *> {};
@@ -80,6 +132,20 @@ public:
         delete kvdb;
         delete ssdb;
     }
+/**
+ * Recalculate all database weightings.
+ */
+    void recalcWeights(void) {
+        for (auto iter : *kvdb) {
+            std::cout << "MDB: recalc'ing strand for key: " << iter.first << "\n";
+            _recalcOneStrand(iter.second);
+        }
+    }
+/**
+ * Insert a key-value pair into the database.
+ * @param k key to insert
+ * @param v value to insert
+ */
     void insertHunk(string k, string v) {
         auto iter = kvdb->find(k);
         if (iter != kvdb->end()) {
@@ -89,11 +155,22 @@ public:
             kvdb->insert({k, new Key {v}});
         }
         std::cout << "MDB: inserted hunk " << k << " = " << v << "\n";
+        recalcWeights();
     }
+/**
+ * Insert a string as a "sentence starter".
+ * This helps to start sentences at the right place, instead
+ * of getting a truncated half-sentence.
+ * @param ss string to insert
+ */
     void insertSentenceStarter(string ss) {
         ssdb->insert(ssdb->end(), ss);
         std::cout << "MDB: inserted sentence starter " << ss << "\n";
     }
+/**
+ * Build a sentence!
+ * @exception std::invalid_argument No sentence starters exist to build a sentence from.
+ */
     string build_sentence(void) {
         if (ssdb->size() == 0)
             throw std::invalid_argument("No sentence starters in database.");
@@ -126,26 +203,23 @@ int main() {
         std::istringstream ss;
         ss.str(sentence);
         std::pair<std::string,std::string> kv {"", ""};
-        std::string tok;
+        std::string tok {""}, ctok {""}, temp;
         int spaces = 0;
-        bool first = true;
-        while (std::getline(ss, tok, ' ')) {
-            if (spaces++ < 2) kv.first = kv.first + (kv.first == "" ? "" : " ") + tok;
-            else kv.second = kv.second + (kv.second == "" ? "" : " ") + tok;
-            if (spaces == 2) {
-                tok = "";
-                if (first) {
-                    first = false;
-                    mdb.insertSentenceStarter(kv.first);
+        while (std::getline(ss, temp, ' ')) {
+            ctok = (ctok == "" ? temp : ctok + " " + temp);
+            if (++spaces == 2) {
+                if (tok == "") {
+                    mdb.insertSentenceStarter(ctok);
                 }
-            }
-            else if (spaces == 4) {
-                tok = "";
+                else {
+                    mdb.insertHunk(tok, ctok);
+                }
+                tok = ctok;
                 spaces = 0;
-                mdb.insertHunk(kv.first, kv.second);
-                kv = {"", ""};
+                ctok = "";
             }
         }
+        if (ctok != "") mdb.insertHunk(tok, ctok);
     }
     std::cin.clear();
     std::cout << "\nSentence input complete.\nGenerating sentences (Enter for new, Control-D to stop)\n\n";
